9f602d547fe3c539221065a0d4ba3107cf9691da
picodexter <picodexter10@gmail.com>
2017-04-26 18:31:39 +0200

    Add bundle class

---
+++ PcdxParameterEncryptionBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * PcdxParameterEncryptionBundle.
+ */
+class PcdxParameterEncryptionBundle extends Bundle
+{
+}

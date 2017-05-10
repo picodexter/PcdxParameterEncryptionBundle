d26530b2935bef99aaa8ec6c1b084dcc33a7f34e
picodexter <picodexter10@gmail.com>
2017-05-10 20:52:46 +0200

    Add implementation of Caesar cipher

---
+++ Encryption/CaesarCipher.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Encryption;
+
+/**
+ * CaesarCipher.
+ */
+class CaesarCipher implements CaesarCipherInterface
+{
+    /**
+     * @var string All the characters that should be handled by the Caesar cipher. Lower case only.
+     */
+    const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+
+    /**
+     * @inheritDoc
+     */
+    public function apply($inputText, $rotationAmount)
+    {
+        $rotationAmount = $this->getValidRotationAmount($rotationAmount);
+
+        $outputText = '';
+
+        for ($i = 0; $i < mb_strlen($inputText); $i++) {
+            $outputText .= $this->getRotatedCharacter(mb_substr($inputText, $i, 1), $rotationAmount);
+        }
+
+        return $outputText;
+    }
+
+    /**
+     * Get alphabet size.
+     *
+     * @return int
+     */
+    private function getAlphabetSize()
+    {
+        return mb_strlen(self::ALPHABET);
+    }
+
+    /**
+     * Get rotated character.
+     *
+     * @param string $character
+     * @param int    $rotationAmount
+     * @return string
+     */
+    private function getRotatedCharacter($character, $rotationAmount)
+    {
+        $alphabetSize = $this->getAlphabetSize();
+
+        $positionInAlphabet = mb_stripos(self::ALPHABET, $character);
+
+        if ($positionInAlphabet === false) {
+            $rotatedCharacter = $character;
+        } else {
+            $newCharacterOffset = (($positionInAlphabet + $rotationAmount + $alphabetSize) % $alphabetSize)
+                - $positionInAlphabet;
+
+            $rotatedCharacter = chr(ord($character) + $newCharacterOffset);
+        }
+
+        return $rotatedCharacter;
+    }
+
+    /**
+     * Get valid rotation amount.
+     *
+     * @param int $rotationAmount
+     * @return int
+     */
+    private function getValidRotationAmount($rotationAmount)
+    {
+        $rotationAmount = (int) $rotationAmount;
+
+        $alphabetSize = $this->getAlphabetSize();
+
+        if ($rotationAmount < 0) {
+            $rotationAmount += (int) ceil(-1 * $rotationAmount / $alphabetSize) * $alphabetSize;
+        }
+
+        return $rotationAmount;
+    }
+}
---
+++ Tests/Encryption/CaesarCipherTest.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Tests\Encryption;
+
+use Picodexter\ParameterEncryptionBundle\Encryption\CaesarCipher;
+
+class CaesarCipherTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CaesarCipher
+     */
+    private $cipher;
+
+    /**
+     * PHPUnit: setUp.
+     */
+    public function setUp()
+    {
+        $this->cipher = new CaesarCipher();
+    }
+
+    /**
+     * PHPUnit: tearDown.
+     */
+    public function tearDown()
+    {
+        $this->cipher = null;
+    }
+
+    /**
+     * @param string $inputText
+     * @param int    $rotationAmount
+     * @param string $expectedOutputText
+     *
+     * @dataProvider provideApplySuccessData
+     */
+    public function testApplySuccess($inputText, $rotationAmount, $expectedOutputText)
+    {
+        $outputText = $this->cipher->apply($inputText, $rotationAmount);
+
+        $this->assertSame($expectedOutputText, $outputText);
+    }
+
+    /**
+     * Data provider.
+     */
+    public function provideApplySuccessData()
+    {
+        return [
+            'complete alphabet, rotation = 13' => [
+                'abcdefghijklmnopqrstuvwxyz',
+                13,
+                'nopqrstuvwxyzabcdefghijklm',
+            ],
+            'complete alphabet, rotation = -13' => [
+                'abcdefghijklmnopqrstuvwxyz',
+                13,
+                'nopqrstuvwxyzabcdefghijklm',
+            ],
+            'alphabet with upper case, rotation = 13' => [
+                'abcDEFGhijklMNOPQrstUvWxyZ',
+                13,
+                'nopQRSTuvwxyZABCDefgHiJklM',
+            ],
+            'no alphabet characters, rotation = 13' => [
+                '.,-àäü @!€',
+                13,
+                '.,-àäü @!€',
+            ],
+            'mixed, rotation = 10' => [
+                'abcdefz áù words 😃',
+                10,
+                'klmnopj áù gybnc 😃',
+            ],
+            'non-numeric rotation' => [
+                'abcdef',
+                'notanint',
+                'abcdef',
+            ],
+            'non-numeric rotation cast to int' => [
+                'abcdef',
+                '3notanint',
+                'defghi',
+            ],
+            'rotation = 0' => [
+                'abcdef',
+                0,
+                'abcdef',
+            ],
+            'rotation > alphabet size' => [
+                'abcdef',
+                100,
+                'wxyzab',
+            ],
+            'rotation < 0' => [
+                'abcdef',
+                -100,
+                'efghij',
+            ],
+            'empty input text' => [
+                '',
+                13,
+                '',
+            ],
+        ];
+    }
+}

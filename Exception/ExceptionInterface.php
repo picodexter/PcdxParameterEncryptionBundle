23819aad250dd1054eeead8d5de8455484df8e10
picodexter <picodexter10@gmail.com>
2017-05-15 00:56:31 +0200

    Add base Exception interface

---
+++ Exception/ExceptionInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Exception;
+
+/**
+ * ExceptionInterface.
+ */
+interface ExceptionInterface
+{
+}

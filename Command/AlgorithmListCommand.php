180e9022c9868a91da517031ec52dae6f32c3ca7
picodexter <picodexter10@gmail.com>
2017-05-20 21:11:23 +0200

    Add algorithm list command

---
+++ Command/AlgorithmListCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Command;
+
+use Picodexter\ParameterEncryptionBundle\Console\Dispatcher\AlgorithmListDispatcherInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * AlgorithmListCommand.
+ */
+class AlgorithmListCommand extends Command
+{
+    /**
+     * @var AlgorithmListDispatcherInterface
+     */
+    private $dispatcher;
+
+    /**
+     * Constructor.
+     *
+     * @param AlgorithmListDispatcherInterface $dispatcher
+     */
+    public function __construct(AlgorithmListDispatcherInterface $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+
+        parent::__construct();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this->setName('pcdx-parameter-encryption:algorithm:list')
+            ->setDescription('List configured algorithms.');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->dispatcher->dispatchToProcessor($input, $output);
+    }
+}
---
+++ Tests/Command/AlgorithmListCommandTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Tests\Command;
+
+use Picodexter\ParameterEncryptionBundle\Command\AlgorithmListCommand;
+use Picodexter\ParameterEncryptionBundle\Console\Dispatcher\AlgorithmListDispatcherInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AlgorithmListCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRunSuccess()
+    {
+        $dispatcher = $this->createAlgorithmListDispatcherInterfaceMock();
+        $input = $this->createInputInterfaceMock();
+        $output = $this->createOutputInterfaceMock();
+
+        $command = new AlgorithmListCommand($dispatcher);
+
+        $dispatcher->expects($this->once())
+            ->method('dispatchToProcessor')
+            ->with(
+                $this->identicalTo($input),
+                $this->identicalTo($output)
+            );
+
+        $command->run($input, $output);
+    }
+
+    /**
+     * Create mock for AlgorithmListDispatcherInterface.
+     *
+     * @return AlgorithmListDispatcherInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createAlgorithmListDispatcherInterfaceMock()
+    {
+        return $this->getMockBuilder(AlgorithmListDispatcherInterface::class)->getMock();
+    }
+
+    /**
+     * Create mock for InputInterface.
+     *
+     * @return InputInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createInputInterfaceMock()
+    {
+        return $this->getMockBuilder(InputInterface::class)->getMock();
+    }
+
+    /**
+     * Create mock for OutputInterface.
+     *
+     * @return OutputInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createOutputInterfaceMock()
+    {
+        return $this->getMockBuilder(OutputInterface::class)->getMock();
+    }
+}

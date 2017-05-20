bb1e90b86996e0fda14da6f4a36cc75432c707c2
picodexter <picodexter10@gmail.com>
2017-05-21 01:13:13 +0200

    Add InvalidConsoleInputException

---
+++ Exception/Console/InvalidConsoleInputException.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Exception\Console;
+
+use Picodexter\ParameterEncryptionBundle\Exception\InvalidArgumentException;
+
+/**
+ * InvalidConsoleInputException.
+ */
+class InvalidConsoleInputException extends InvalidArgumentException
+{
+}

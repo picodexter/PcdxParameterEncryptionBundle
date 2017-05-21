a6f0a1b504412792a38aa147b895cbe3d8971076
picodexter <picodexter10@gmail.com>
2017-05-22 00:31:09 +0200

    Add encrypt request transfer object

---
+++ Console/Request/EncryptRequest.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Console\Request;
+
+use Picodexter\ParameterEncryptionBundle\Console\Helper\QuestionAskerInterface;
+
+/**
+ * EncryptRequest.
+ */
+class EncryptRequest
+{
+    /**
+     * @var string
+     */
+    private $algorithmId;
+
+    /**
+     * @var string|null
+     */
+    private $key;
+
+    /**
+     * @var bool
+     */
+    private $keyProvided;
+
+    /**
+     * @var QuestionAskerInterface
+     */
+    private $plaintextQuestionAsker;
+
+    /**
+     * @var string
+     */
+    private $plaintextValue;
+
+    /**
+     * Constructor.
+     *
+     * @param string                 $algorithmId
+     * @param string|null            $key
+     * @param bool                   $keyProvided
+     * @param QuestionAskerInterface $plaintextAsker
+     * @param string                 $plaintextValue
+     */
+    public function __construct(
+        $algorithmId,
+        $key,
+        $keyProvided,
+        QuestionAskerInterface $plaintextAsker,
+        $plaintextValue
+    ) {
+        $this->algorithmId = $algorithmId;
+        $this->key = $key;
+        $this->keyProvided = (bool) $keyProvided;
+        $this->plaintextQuestionAsker = $plaintextAsker;
+        $this->plaintextValue = $plaintextValue;
+    }
+
+    /**
+     * Getter: algorithmId.
+     *
+     * @return string
+     */
+    public function getAlgorithmId()
+    {
+        return $this->algorithmId;
+    }
+
+    /**
+     * Getter: key.
+     *
+     * @return string|null
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * Getter: keyProvided.
+     *
+     * @return bool
+     */
+    public function isKeyProvided()
+    {
+        return $this->keyProvided;
+    }
+
+    /**
+     * Getter: plaintextQuestionAsker.
+     *
+     * @return QuestionAskerInterface
+     */
+    public function getPlaintextQuestionAsker()
+    {
+        return $this->plaintextQuestionAsker;
+    }
+
+    /**
+     * Getter: plaintextValue.
+     *
+     * @return string
+     */
+    public function getPlaintextValue()
+    {
+        return $this->plaintextValue;
+    }
+}
---
+++ Tests/Console/Request/EncryptRequestTest.php
@@ -0,0 +1,163 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Tests\Console\Request;
+
+use Picodexter\ParameterEncryptionBundle\Console\Helper\QuestionAskerInterface;
+use Picodexter\ParameterEncryptionBundle\Console\Request\EncryptRequest;
+
+class EncryptRequestTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructorKeyProvidedSuccessCast()
+    {
+        $requestData = $this->getDummyEncryptRequestData();
+
+        $requestData['keyProvided'] = 'not a boolean';
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertTrue($request->isKeyProvided());
+
+        $requestData['keyProvided'] = 0;
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertFalse($request->isKeyProvided());
+    }
+
+    public function testGetAlgorithmIdSuccess()
+    {
+        $requestData = $this->getDummyEncryptRequestData();
+
+        $requestData['algorithmId'] = 'sample_algo_01';
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertSame($requestData['algorithmId'], $request->getAlgorithmId());
+
+        $requestData['algorithmId'] = 'sample_algo_02';
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertSame($requestData['algorithmId'], $request->getAlgorithmId());
+    }
+
+    public function testGetKeySuccess()
+    {
+        $requestData = $this->getDummyEncryptRequestData();
+
+        $requestData['key'] = 'super secret key';
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertSame($requestData['key'], $request->getKey());
+
+        $requestData['key'] = 'other secret key';
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertSame($requestData['key'], $request->getKey());
+    }
+
+    public function testIsKeyProvidedSuccess()
+    {
+        $requestData = $this->getDummyEncryptRequestData();
+
+        $requestData['keyProvided'] = true;
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertSame($requestData['keyProvided'], $request->isKeyProvided());
+
+        $requestData['keyProvided'] = false;
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertSame($requestData['keyProvided'], $request->isKeyProvided());
+    }
+
+    public function testGetPlaintextQuestionAskerSuccess()
+    {
+        $requestData = $this->getDummyEncryptRequestData();
+
+        $requestData['plaintextAsker'] = $this->createQuestionAskerInterfaceMock();
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertSame($requestData['plaintextAsker'], $request->getPlaintextQuestionAsker());
+
+        $requestData['plaintextAsker'] = $this->createQuestionAskerInterfaceMock();
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertSame($requestData['plaintextAsker'], $request->getPlaintextQuestionAsker());
+    }
+
+    public function testGetPlaintextValueSuccess()
+    {
+        $requestData = $this->getDummyEncryptRequestData();
+
+        $requestData['plaintextValue'] = 'encrypted 1';
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertSame($requestData['plaintextValue'], $request->getPlaintextValue());
+
+        $requestData['plaintextValue'] = 'encrypted 2';
+
+        $request = $this->createEncryptRequestWithData($requestData);
+
+        $this->assertSame($requestData['plaintextValue'], $request->getPlaintextValue());
+    }
+
+    /**
+     * Create encrypt request with data.
+     *
+     * @param array $requestData
+     * @return EncryptRequest
+     */
+    private function createEncryptRequestWithData(array $requestData)
+    {
+        return new EncryptRequest(
+            $requestData['algorithmId'],
+            $requestData['key'],
+            $requestData['keyProvided'],
+            $requestData['plaintextAsker'],
+            $requestData['plaintextValue']
+        );
+    }
+
+    /**
+     * Create mock for QuestionAskerInterface.
+     *
+     * @return QuestionAskerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createQuestionAskerInterfaceMock()
+    {
+        return $this->getMockBuilder(QuestionAskerInterface::class)->disableOriginalConstructor()->getMock();
+    }
+
+    /**
+     * Get dummy encrypt request data.
+     *
+     * @return array
+     */
+    private function getDummyEncryptRequestData()
+    {
+        return [
+            'algorithmId'    => 'algo_01',
+            'key'            => 'secret key',
+            'keyProvided'    => true,
+            'plaintextAsker' => $this->createQuestionAskerInterfaceMock(),
+            'plaintextValue' => 'encrypted text',
+        ];
+    }
+}

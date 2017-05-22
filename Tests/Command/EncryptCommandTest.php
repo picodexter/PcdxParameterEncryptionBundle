0e2f7eeb0f377b646a5168ddb396a1444a0de7ce
picodexter <picodexter10@gmail.com>
2017-05-22 20:32:33 +0200

    Add encrypt command

---
+++ Tests/Command/EncryptCommandTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Tests\Command;
+
+use Picodexter\ParameterEncryptionBundle\Command\EncryptCommand;
+use Picodexter\ParameterEncryptionBundle\Console\Dispatcher\EncryptDispatcherInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class EncryptCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecuteSuccess()
+    {
+        $dispatcher = $this->createEncryptDispatcherInterfaceMock();
+
+        $command = new EncryptCommand($dispatcher);
+
+        $input = $this->createInputInterfaceMock();
+        $output = $this->createOutputInterfaceMock();
+
+        $dispatcher->expects($this->once())
+            ->method('dispatchToProcessor')
+            ->with(
+                $this->identicalTo($input),
+                $this->identicalTo($output)
+            );
+
+        $command->run($input, $output);
+    }
+
+    /**
+     * Create mock for EncryptDispatcherInterface.
+     *
+     * @return EncryptDispatcherInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createEncryptDispatcherInterfaceMock()
+    {
+        return $this->getMockBuilder(EncryptDispatcherInterface::class)->getMock();
+    }
+
+    /**
+     * Create mock for InputInterface.
+     *
+     * @return InputInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createInputInterfaceMock()
+    {
+        return $this->getMockBuilder(InputInterface::class)->getMock();
+    }
+
+    /**
+     * Create mock for OutputInterface.
+     *
+     * @return OutputInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createOutputInterfaceMock()
+    {
+        return $this->getMockBuilder(OutputInterface::class)->getMock();
+    }
+}

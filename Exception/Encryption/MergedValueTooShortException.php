811d58172686cbaffc5a9542fbaaf4fd528d98f5
picodexter <picodexter10@gmail.com>
2017-06-01 01:59:10 +0200

    Add MergedValueTooShortException

---
+++ Exception/Encryption/MergedValueTooShortException.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Exception\Encryption;
+
+use Throwable;
+
+/**
+ * MergedValueTooShortException.
+ */
+class MergedValueTooShortException extends EncryptionException
+{
+    /**
+     * Constructor.
+     *
+     * @param Throwable|null $previous
+     */
+    public function __construct(Throwable $previous = null)
+    {
+        parent::__construct('Merged value is too short', 0, $previous);
+    }
+}

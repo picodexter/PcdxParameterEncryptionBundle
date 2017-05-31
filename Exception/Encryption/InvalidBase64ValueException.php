8b9e641beb33a341ee20f346db20cbc46f009f30
picodexter <picodexter10@gmail.com>
2017-06-01 00:55:34 +0200

    Add InvalidBase64ValueException

---
+++ Exception/Encryption/InvalidBase64ValueException.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Exception\Encryption;
+
+use Throwable;
+
+/**
+ * InvalidBase64ValueException.
+ */
+class InvalidBase64ValueException extends EncryptionException
+{
+    /**
+     * Constructor.
+     *
+     * @param Throwable|null $previous
+     */
+    public function __construct(Throwable $previous = null)
+    {
+        parent::__construct('Invalid Base64 value', 0, $previous);
+    }
+}

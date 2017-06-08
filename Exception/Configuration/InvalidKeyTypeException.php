2fc3ef268715b03b5a1e5ef625de7db70ddc864a
picodexter <picodexter10@gmail.com>
2017-06-09 00:20:09 +0200

    Add InvalidKeyTypeException

---
+++ Exception/Configuration/InvalidKeyTypeException.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Exception\Configuration;
+
+use Throwable;
+
+/**
+ * InvalidKeyTypeException.
+ */
+class InvalidKeyTypeException extends InvalidAlgorithmConfigurationException
+{
+    /**
+     * Constructor.
+     *
+     * @param string         $keyTypeName
+     * @param Throwable|null $previous
+     */
+    public function __construct($keyTypeName, Throwable $previous = null)
+    {
+        parent::__construct($previous);
+
+        $this->message = sprintf('Invalid key type: %s', $keyTypeName);
+    }
+}

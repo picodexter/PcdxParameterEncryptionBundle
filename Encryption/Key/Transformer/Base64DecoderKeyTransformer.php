28984d886d205e9ab4c43c37b320d31765455dc9
picodexter <picodexter10@gmail.com>
2017-06-16 15:27:36 +0200

    Add key transformer for base64 encoded value

---
+++ Encryption/Key/Transformer/Base64DecoderKeyTransformer.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Encryption\Key\Transformer;
+
+use Picodexter\ParameterEncryptionBundle\Configuration\Key\KeyConfiguration;
+use Picodexter\ParameterEncryptionBundle\Encryption\Value\Encoding\Base64DecoderInterface;
+
+/**
+ * Base64DecoderKeyTransformer.
+ */
+class Base64DecoderKeyTransformer implements KeyTransformerInterface
+{
+    /**
+     * @var Base64DecoderInterface
+     */
+    private $base64Decoder;
+
+    /**
+     * Constructor.
+     *
+     * @param Base64DecoderInterface $base64Decoder
+     */
+    public function __construct(Base64DecoderInterface $base64Decoder)
+    {
+        $this->base64Decoder = $base64Decoder;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function appliesFor($key, KeyConfiguration $keyConfig)
+    {
+        return $keyConfig->isBase64Encoded();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function transform($key, KeyConfiguration $keyConfig)
+    {
+        return $this->base64Decoder->decode($key);
+    }
+}
---
+++ Tests/Encryption/Key/Transformer/Base64DecoderKeyTransformerTest.php
@@ -0,0 +1,123 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Tests\Encryption\Key\Transformer;
+
+use Picodexter\ParameterEncryptionBundle\Configuration\Key\KeyConfiguration;
+use Picodexter\ParameterEncryptionBundle\Encryption\Key\Transformer\Base64DecoderKeyTransformer;
+use Picodexter\ParameterEncryptionBundle\Encryption\Value\Encoding\Base64DecoderInterface;
+
+class Base64DecoderKeyTransformerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Base64DecoderInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $base64Decoder;
+
+    /**
+     * @var Base64DecoderKeyTransformer
+     */
+    private $transformer;
+
+    /**
+     * PHPUnit: setUp.
+     */
+    public function setUp()
+    {
+        $this->base64Decoder = $this->createBase64DecoderInterfaceMock();
+
+        $this->transformer = new Base64DecoderKeyTransformer($this->base64Decoder);
+    }
+
+    /**
+     * PHPUnit: tearDown.
+     */
+    public function tearDown()
+    {
+        $this->transformer = null;
+        $this->base64Decoder = null;
+    }
+
+    /**
+     * @param bool $base64Encoded
+     * @param bool $expectedResult
+     *
+     * @dataProvider provideAppliesForData
+     */
+    public function testAppliesForSuccess($base64Encoded, $expectedResult)
+    {
+        $key = 'some key';
+
+        $keyConfig = $this->createKeyConfiguration();
+
+        $keyConfig->setBase64Encoded($base64Encoded);
+
+        $result = $this->transformer->appliesFor($key, $keyConfig);
+
+        $this->assertSame($expectedResult, $result);
+    }
+
+    /**
+     * Data provider.
+     */
+    public function provideAppliesForData()
+    {
+        return [
+            'match' => [
+                true,
+                true,
+            ],
+            'no match' => [
+                false,
+                false,
+            ],
+        ];
+    }
+
+    public function testTransformSuccess()
+    {
+        $key = 'some key';
+        $prepTransfKey = 'transformed key';
+
+        $keyConfig = $this->createKeyConfiguration();
+
+        $this->base64Decoder->expects($this->once())
+            ->method('decode')
+            ->with($this->identicalTo($key))
+            ->will($this->returnValue($prepTransfKey));
+
+        $transformedKey = $this->transformer->transform($key, $keyConfig);
+
+        $this->assertSame($prepTransfKey, $transformedKey);
+    }
+
+    /**
+     * Create mock for Base64DecoderInterface.
+     *
+     * @return Base64DecoderInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function createBase64DecoderInterfaceMock()
+    {
+        return $this->getMockBuilder(Base64DecoderInterface::class)->getMock();
+    }
+
+    /**
+     * Create key configuration.
+     *
+     * @return KeyConfiguration
+     */
+    private function createKeyConfiguration()
+    {
+        $keyConfig = new KeyConfiguration();
+
+        return $keyConfig;
+    }
+}

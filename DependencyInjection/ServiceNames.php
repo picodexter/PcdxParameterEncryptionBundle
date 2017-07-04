ae521f94afa30ad4557fd6bde14e1c51864753fb
picodexter <picodexter10@gmail.com>
2017-07-04 18:30:10 +0200

    Add service name for bundle configuration service definition processor

--- DependencyInjection/ServiceNames.php
+++ DependencyInjection/ServiceNames.php
@@ -22,6 +22,8 @@ final class ServiceNames
         = 'pcdx_parameter_encryption.configuration.algorithm_configuration_container';
     const ALGORITHM_CONFIGURATION_FACTORY = 'pcdx_parameter_encryption.configuration.algorithm_configuration_factory';
     const ALGORITHM_CONFIGURATION_PREFIX = 'pcdx_parameter_encryption.configuration.algorithm_configuration.';
+    const BUNDLE_CONFIGURATION_SERVICE_DEFINITION_PROCESSOR
+        = 'pcdx_parameter_encryption.dependency_injection.bundle_configuration.service_definition.processor';
     const KEY_CONFIGURATION_FACTORY = 'pcdx_parameter_encryption.configuration.key.configuration_factory';
     const KEY_FETCHER = 'pcdx_parameter_encryption.encryption.key.fetcher';
     const KEY_VALIDATOR_NOT_EMPTY = 'pcdx_parameter_encryption.encryption.key.validator.not_empty';

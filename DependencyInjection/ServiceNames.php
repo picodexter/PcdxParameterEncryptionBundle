e40c5ab7e664592dda943203be5b58a520beb333
picodexter <picodexter10@gmail.com>
2017-07-05 01:12:48 +0200

    Add service name for bundle configuration service definition rewriter registry

--- DependencyInjection/ServiceNames.php
+++ DependencyInjection/ServiceNames.php
@@ -24,6 +24,8 @@ final class ServiceNames
     const ALGORITHM_CONFIGURATION_PREFIX = 'pcdx_parameter_encryption.configuration.algorithm_configuration.';
     const BUNDLE_CONFIGURATION_SERVICE_DEFINITION_PROCESSOR
         = 'pcdx_parameter_encryption.dependency_injection.bundle_configuration.service_definition.processor';
+    const BUNDLE_CONFIGURATION_SERVICE_DEFINITION_REWRITER_REGISTRY
+        = 'pcdx_parameter_encryption.dependency_injection.bundle_configuration.service_definition.rewriter.registry';
     const KEY_CONFIGURATION_FACTORY = 'pcdx_parameter_encryption.configuration.key.configuration_factory';
     const KEY_FETCHER = 'pcdx_parameter_encryption.encryption.key.fetcher';
     const KEY_VALIDATOR_NOT_EMPTY = 'pcdx_parameter_encryption.encryption.key.validator.not_empty';

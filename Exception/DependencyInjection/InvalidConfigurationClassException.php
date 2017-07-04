92dea1e4fc9c29922afe2c86a531b2733decc895
picodexter <picodexter10@gmail.com>
2017-07-04 17:18:09 +0200

    Add InvalidConfigurationClassException

---
+++ Exception/DependencyInjection/InvalidConfigurationClassException.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the PcdxParameterEncryptionBundle package.
+ *
+ * (c) picodexter <https://picodexter.io/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Picodexter\ParameterEncryptionBundle\Exception\DependencyInjection;
+
+use Throwable;
+
+/**
+ * InvalidConfigurationClassException.
+ */
+class InvalidConfigurationClassException extends DependencyInjectionException
+{
+    /**
+     * Constructor.
+     *
+     * @param string         $className
+     * @param Throwable|null $previous
+     */
+    public function __construct($className, Throwable $previous = null)
+    {
+        parent::__construct(sprintf('Invalid Symfony configuration class "%s"', $className), 0, $previous);
+    }
+}
